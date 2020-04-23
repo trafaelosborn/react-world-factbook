88213adc928c72bcef2e9b18df713e265bc2eb9d
Taylor Osborn <osborn.taylor@gmail.com>
2020-04-23 08:56:00 -0700

    Update Main.js

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,19 +6,27 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          selectedCountry: "bahrain"
+          selectedCountry: "Afghanistan"
         };
       }
 
     render () {
+      const selectedCountryStr = JSON.stringify(this.state.selectedCountry)
+      console.log(selectedCountryStr);
+      
+      const selectedCountry = JSON.parse(selectedCountryStr);
+      console.log(selectedCountry);
         return (
+         
             <div>
             {Factbook.map((dataDetail, index)=>{
-                console.log(dataDetail.countries)
+                
 
                 return ( 
+                  
                     <div>
-                <h1>{dataDetail.countries.united_kingdom.data.name}</h1>
+                <h1>{selectedCountry}</h1>
+                <h1>{dataDetail.countries.bahrain.data.name} </h1>
                 <p>{dataDetail.countries.mexico.data.introduction.background}</p>
                 <p>{dataDetail.countries.afghanistan.data.communications.broadcast_media}</p>
                     </div>                        
